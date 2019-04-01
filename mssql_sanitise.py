1b7d2a0e6c8252c83db1e920df4572d6e3eec787
Draper1 <andy.draper@awdconsultancy.uk>
2019-04-01 10:58:43 +0100

    Correct Update Syntax

--- mssql_sanitise.py
+++ mssql_sanitise.py
@@ -21,7 +21,8 @@ print "Connecting to "+sql_host+"\\"+sql_instance+" and DB: "+sql_db_name+"-"+sq
 cnxn = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server="+sql_host+"\\"+sql_instance+",1433;Database="+sql_db_name+"-"+sql_mount_suffix+";UID="+sql_user+";PWD="+sql_pass)
 
 cursor = cnxn.cursor()
-cursor.execute('UPDATE Person.EmailAddress SET EmailAddress=\'xxxxxx@xxxxxx.com\' FROM Person.EmailAddress')
+
+cursor.execute('UPDATE A SET A.EmailAddress=\'xxxxxx@xxxxxx.com\' FROM Person.EmailAddress A')
 cursor.execute('SELECT TOP 10 [EmailAddress] FROM Person.EmailAddress')
 for row in cursor:
     print('row = %r' % (row,))
