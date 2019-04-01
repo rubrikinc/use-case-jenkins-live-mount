d010442480db8ff7d0dc723ffe6159ed3702e42d
Draper1 <andy.draper@awdconsultancy.uk>
2019-04-01 12:03:57 +0100

    Change to SPROC method

--- mssql_sanitise.py
+++ mssql_sanitise.py
@@ -17,13 +17,27 @@ sql_db_name = args.sql_db
 sql_mount_suffix = args.suffix
 sql_user = args.username
 sql_pass = args.password
+
 print "Connecting to "+sql_host+"\\"+sql_instance+" and DB: "+sql_db_name+"-"+sql_mount_suffix
 cnxn = pyodbc.connect("Driver={ODBC Driver 13 for SQL Server};Server="+sql_host+"\\"+sql_instance+",1433;Database="+sql_db_name+"-"+sql_mount_suffix+";UID="+sql_user+";PWD="+sql_pass)
 
+sql_sp = ('CREATE OR ALTER PROC USP_DataMasking (@TBName varchar(50), @ColumnName VARCHAR(200), @ColumnValue VARCHAR(200))'
+          'AS'
+          'BEGIN'
+          'DECLARE @STRSQL VARCHAR(MAX)'
+          'SET @STRSQL = CONCAT(\'UPDATE A SET A.\',@ColumnName, \'=\'\'\',@ColumnValue,\'\'\' FROM \',@TBName, \' A\')'
+          'EXEC(@STRSQL)'
+          'END'
+          'GO')
+
+print sql_sp
+
 cursor = cnxn.cursor()
 
-cursor.execute('UPDATE A SET A.EmailAddress=\'xxxxxx@xxxxxx.com\' FROM Person.EmailAddress A')
-cursor.execute('SELECT TOP 10 [EmailAddress] FROM Person.EmailAddress')
+print "Executing SQL Sanitise Commands"
+cursor.execute(sql_sp)
+cursor.execute('USP_DataMasking \'Person.EmailAddress\', \'EmailAddress\',\'xxxxxx@xxxxxx.com\'')
+
 for row in cursor:
     print('row = %r' % (row,))
 
