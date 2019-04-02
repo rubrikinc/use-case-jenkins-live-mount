668daf616e5df87f6f1358459ce52dd5fa2af59c
Draper1 <andy.draper@awdconsultancy.uk>
2019-04-02 19:15:03 +0100

    Updated Web Config

--- data.php
+++ data.php
@@ -5,13 +5,12 @@
     <body>
         <?php
         $myDatabase = "RUBRIK_LIVEMOUNT_DB";
-        $link = mssql_connect("RUBRIK_SERVER", SQL_USER, SQL_PASSWORD);
+        $link = mssql_connect("RBK_SQL_HOST\RBK_SQL_INSTANCE", 'SQL_USER', 'SQL_PASSWORD');
         if (!$link || !mssql_select_db($myDatabase, $link)) {
             die('Unable to connect or select database!');
         }
         $query = "SELECT EmailAddressID, EmailAddress, ModifiedDate FROM Person.EmailAddress";
         $result = mssql_query($query);
-
         $o = '<table id="myTable">
           <thead>
           <tr>
@@ -19,12 +18,10 @@
           <th>Email Address</th>
           </tr>
           </thead><tbody>';
-
         while ( $record = mssql_fetch_array($result) )
         {
           $o .= '<tr><td>'.$record ['EmailAddressID'].'</td><td>'.$record ['EmailAddress'].'</td></tr>';
         }
-
         $o .= '</tbody></table>';
         echo $o;
         mssql_free_result($query);
